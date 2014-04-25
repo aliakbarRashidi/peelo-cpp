06ae2fdf1a8c21696a06c8c6b970f53f1d4d12c9
cappu <da.cappu@gmail.com>
2014-04-25 11:44:01 +0300

    chrono related enums

---
+++ include/peelo/chrono/month.hpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014, peelo.net
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef PEELO_CHRONO_MONTH_HPP_GUARD
+#define PEELO_CHRONO_MONTH_HPP_GUARD
+
+namespace peelo
+{
+    enum month_e
+    {
+        month_jan = 1,
+        month_feb = 2,
+        month_mar = 3,
+        month_apr = 4,
+        month_may = 5,
+        month_jun = 6,
+        month_jul = 7,
+        month_aug = 8,
+        month_sep = 9,
+        month_oct = 10,
+        month_nov = 11,
+        month_dec = 12
+    };
+}
+
+#endif /* !PEELO_CHRONO_MONTH_HPP_GUARD */

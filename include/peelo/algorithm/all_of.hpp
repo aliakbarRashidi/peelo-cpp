239d0513045e52ad0a683b567b663b40dcf44ffe
Rauli Laine <rauli.laine@gmail.com>
2014-11-08 15:13:08 +0200

    Add more algorithm implementations.
    Basically copies you find in standard C++ library, but nonethless useful
    things to have.

---
+++ include/peelo/algorithm/all_of.hpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2014, peelo.net
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef PEELO_ALGORITHM_ALL_OF_HPP_GUARD
+#define PEELO_ALGORITHM_ALL_OF_HPP_GUARD
+
+#include <peelo/algorithm/find_if_not.hpp>
+
+namespace peelo
+{
+    template< class InputIt, class UnaryPredicate >
+    bool all_of(InputIt first, InputIt last, UnaryPredicate p)
+    {
+        return find_if_not(first, last, p) == last;
+    }
+}
+
+#endif /* !PEELO_ALGORITHM_ALL_OF_HPP_GUARD */
